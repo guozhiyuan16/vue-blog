c346864b33e1fb91d57bac8b46acf5a086f89dc5
zhiyuan.guo <zhiyuan.guo@bjrxtd.com>
2021-07-26 23:36:56 +0800

    style:新增登录注册modal

--- src/components/Header/HeaderNav.vue
+++ src/components/Header/HeaderNav.vue
@@ -4,7 +4,7 @@
       <a-col :span="4">
         <div id="page-title">
           <a-icon type="setting" theme="filled" :style="{ color: '#08c' }" />
-          <span class='blog-name'>个人博客</span>
+          <span class="blog-name">个人博客</span>
         </div>
       </a-col>
       <a-col :span="20">
@@ -22,6 +22,7 @@
             type="primary"
             ghost
             :style="{ marginRight: '20px' }"
+            @click="showModal"
             >登录
           </a-button>
           <a-button size="small" type="danger" ghost>注册</a-button>
@@ -29,19 +30,67 @@
         <div id="page-menu">
           <a-menu v-model="current" mode="horizontal">
             <a-menu-item key="mail"> <a-icon type="home" />首页 </a-menu-item>
-            <a-menu-item key="app">
-              <a-icon type="edit" />归档
-            </a-menu-item>
-            <a-menu-item key="kind">
-              <a-icon type="folder" />分类
-            </a-menu-item>
-            <a-menu-item key="about">
-              <a-icon type="user" />关于
-            </a-menu-item>
+            <a-menu-item key="app"> <a-icon type="edit" />归档 </a-menu-item>
+            <a-menu-item key="kind"> <a-icon type="folder" />分类 </a-menu-item>
+            <a-menu-item key="about"> <a-icon type="user" />关于 </a-menu-item>
           </a-menu>
         </div>
       </a-col>
     </a-row>
+    <a-modal v-model="visible" title="Login" :footer="null" @ok="handleOk">
+      <a-form
+        id="components-form-demo-normal-login"
+        :form="form"
+        class="login-form"
+        @submit="handleSubmit"
+      >
+        <a-form-item>
+          <a-input
+            v-decorator="[
+              'userName',
+              {
+                rules: [
+                  { required: true, message: 'Please input your username!' },
+                ],
+              },
+            ]"
+            placeholder="Username"
+          >
+            <a-icon
+              slot="prefix"
+              type="user"
+              style="color: rgba(0, 0, 0, 0.25)"
+            />
+          </a-input>
+        </a-form-item>
+        <a-form-item>
+          <a-input
+            v-decorator="[
+              'password',
+              {
+                rules: [
+                  { required: true, message: 'Please input your Password!' },
+                ],
+              },
+            ]"
+            type="password"
+            placeholder="Password"
+          >
+            <a-icon
+              slot="prefix"
+              type="lock"
+              style="color: rgba(0, 0, 0, 0.25)"
+            />
+          </a-input>
+        </a-form-item>
+        <a-button type="primary" block>
+          login
+        </a-button>
+        <a-button block icon="github" :style="{marginTop:'10px'}">
+          github login
+        </a-button>
+      </a-form>
+    </a-modal>
   </div>
 </template>
 
@@ -50,7 +99,28 @@ export default {
   data() {
     return {
       current: ["mail"],
+      visible: false,
     };
+  },
+  beforeCreate() {
+    this.form = this.$form.createForm(this, { name: 'normal_login' });
+  },
+  methods: {
+    showModal() {
+      this.visible = true;
+    },
+    handleOk(e) {
+      console.log(e);
+      this.visible = false;
+    },
+    handleSubmit(e) {
+      e.preventDefault();
+      this.form.validateFields((err, values) => {
+        if (!err) {
+          console.log('Received values of form: ', values);
+        }
+      });
+    },
   },
 };
 </script>
