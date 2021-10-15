1c3e8149cd427ae5ecf0ba0c3005bb80d79b887b
guozhiyuan <zhiyuan.guo@bjrxtd.com>
2021-10-15 17:50:12 +0800

    feat:文章新增页面

--- src/views/admin/add.vue
+++ src/views/admin/add.vue
@@ -1,51 +1,98 @@
 <template>
-  <div>
-    <a-form-model
-      layout="inline"
-      :model="formInline"
-      @submit="handleSubmit"
-      @submit.native.prevent
-    >
-      <a-form-model-item label="关键词">
-        <a-input v-model="formInline.keyword" placeholder="请输入文章关键词" :style="{width:'200px'}"/>
-      </a-form-model-item>
-      <a-form-model-item label="标签">
-        <a-select v-model="formInline.tag" :style="{width:'200px'}">
-          <a-select-option value="Vue"> Vue </a-select-option>
-          <a-select-option value="React"> React </a-select-option>
-        </a-select>
-      </a-form-model-item>
-      <a-form-model-item label="分类">
-        <a-select v-model="formInline.category"  :style="{width:'200px'}">
-          <a-select-option value="Vue"> Vue </a-select-option>
-          <a-select-option value="React"> React </a-select-option>
-        </a-select>
-      </a-form-model-item>
-      <a-form-model-item>
-        <a-button type="primary" html-type="submit"> 检索 </a-button>
-      </a-form-model-item>
-      <a-form-model-item>
-        <a-button type="primary" html-type="submit"> 导出全部文章 </a-button>
-      </a-form-model-item>
-    </a-form-model>
-  </div>
+  <ul class="form-list">
+      <li>
+        <span>标题</span>
+        <span><a-input placeholder="请输入文章标题"/></span>
+      </li>
+      <li>
+        <span>标签</span>
+        <span>
+          <template v-for="tag in tags">
+            <a-checkable-tag
+              :key="tag"
+              :checked="selectedTags.indexOf(tag) > -1"
+              @change="checked => handleChange(tag, checked)"
+            >
+              {{ tag }}
+            </a-checkable-tag>
+          </template>
+          <a-input
+            v-if="inputVisible"
+            ref="input"
+            type="text"
+            size="small"
+            :style="{ width: '78px' }"
+            :value="inputValue"
+            @change="handleInputChange"
+            @blur="handleInputConfirm"
+            @keyup.enter="handleInputConfirm"
+          />
+          <a-tag v-else style="background: #fff; borderStyle: dashed;" @click="showInput">
+            <a-icon type="plus" /> New Tag
+          </a-tag>
+        </span>
+      </li>
+      <li>
+        <span>分类</span>
+        <span>
+          
+        </span>
+      </li>
+  </ul>
 </template>
 <script>
 export default {
   data() {
     return {
-      labelCol: { span: 4 },
-      wrapperCol: { span: 14 },
-      formInline: {
-        keyword: "",
-        tag: "",
-        category: "",
-      },
+      tags: ['Movies', 'Books', 'Music', 'Sports'],
+      selectedTags: [],
+
+      categories:['Node','React','Vue'],
+      selectedcategories:[],
+
+      inputVisible: false,
+      inputValue: '',
     };
   },
   methods: {
-    handleSubmit(e) {
-      console.log(this.formInline);
+    handleChange(tag, checked) {
+      const { selectedTags } = this;
+      const nextSelectedTags = checked
+        ? [...selectedTags, tag]
+        : selectedTags.filter(t => t !== tag);
+      console.log('You are interested in: ', nextSelectedTags);
+      this.selectedTags = nextSelectedTags;
+    },
+
+    handleClose(removedTag) {
+      const tags = this.tags.filter(tag => tag !== removedTag);
+      console.log(tags);
+      this.tags = tags;
+    },
+
+    showInput() {
+      this.inputVisible = true;
+      this.$nextTick(function() {
+        this.$refs.input.focus();
+      });
+    },
+
+    handleInputChange(e) {
+      this.inputValue = e.target.value;
+    },
+
+    handleInputConfirm() {
+      const inputValue = this.inputValue;
+      let tags = this.tags;
+      if (inputValue && tags.indexOf(inputValue) === -1) {
+        tags = [...tags, inputValue];
+      }
+      console.log(tags);
+      Object.assign(this, {
+        tags,
+        inputVisible: false,
+        inputValue: '',
+      });
     },
   },
 };
